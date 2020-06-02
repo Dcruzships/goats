5bf62fcbea8501a77c048d558df664277f35fcda
dcruzships <bvd5889@rit.edu>
2020-06-02 17:16:34 -0500

    Updates

---
+++ precache-manifest.69e5e6e0f31bf89708b98f97d4d468d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65280fa3e09d1be369c3e0e24156d671",
+    "url": "/goats/index.html"
+  },
+  {
+    "revision": "d8c4468057792ba4b77c",
+    "url": "/goats/static/css/main.902d30ae.chunk.css"
+  },
+  {
+    "revision": "f22f282530c3fd2842e4",
+    "url": "/goats/static/js/2.1be90ea8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goats/static/js/2.1be90ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c4468057792ba4b77c",
+    "url": "/goats/static/js/main.876ef46e.chunk.js"
+  },
+  {
+    "revision": "10982dd1999299090ab5",
+    "url": "/goats/static/js/runtime-main.1cc38658.js"
+  }
+]);
