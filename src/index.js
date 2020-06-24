29200f71fa7fd2b662c55df89d6b98a67b37d230
dcruzships <bvd5889@rit.edu>
2020-06-24 16:01:59 -0500

    More layout
    Also fixed scrolling

--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo1_64.png'; // Tell webpack this JS file uses this image
 import './index.scss';
-import { Anchor, Box, Grommet, Header, Main, Paragraph, Heading, Nav, Image, Carousel, Video } from "grommet";
+import { Anchor, Box, Grommet, Grid, Header, Paragraph, Heading, Nav, Image, Carousel, Video, Collapsible, Text } from "grommet";
 import { grommet } from "grommet/themes";
 
-import Scroll from "react-scroll";
-let scroll = Scroll.animateScroll;
+var Scroll   = require('react-scroll');
+var scroller = Scroll.scroller;
 
 const items = [
   { label: "About", href: "aboutPage" },
-  { label: "Animals", href: "#" },
-  { label: "Stories", href: "#" },
-  { label: "Contact", href: "#" }
+  { label: "Animals", href: "animalsPage" },
+  { label: "Stories", href: "storiesPage" },
+  { label: "Contact", href: "contactPage" }
 ];
 
-function App()
+class App extends React.Component
 {
-  return(
-    <Grommet theme={grommet}>
-      <Box height="100vh">
-        <Navbar />
-        <Slides />
-      </Box>
-      <Box height="100vh" pad="large" id="aboutPage" name="aboutPage">
-        <About />
-      </Box>
-    </Grommet>
-  );
+  render() {
+    return(
+      <Grommet theme={grommet}>
+        <Box height="100vh">
+          <Navbar />
+          <Slides />
+        </Box>
+        <Box height="100vh" pad="large" id="aboutPage" name="aboutPage" background="neutral-1">
+          <About />
+        </Box>
+        <Box height="100vh" pad="large" id="animalsPage" name="animalsPage">
+          <Animals />
+        </Box>
+      </Grommet>
+    );
+  }
 }
 
 class Navbar extends React.Component
 {
-  handleScroll(dest) {
-    scroll.scrollTo(1000);
+  handleScroll = (dest) => {
+    scroller.scrollTo(dest, {
+      duration: 1500,
+      smooth: true,
+    });
+
+    console.log(dest);
   }
 
   render() {
@@ -80,6 +90,57 @@ const About = () => {
       <Paragraph>More things</Paragraph>
       <Paragraph>More things</Paragraph>
       <Paragraph>More things</Paragraph>
+    </Box>
+  )
+}
+
+const Animals = () => {
+  return(
+    <Carousel fill>
+      <GoatGrid name1="goat1" name2="goat2" name3="goat3"/>
+      <GoatGrid name1="goat4" name2="goat5" name3="goat6"/>
+      <GoatGrid name1="goat7" name2="goat8" name3="goat9"/>
+    </Carousel>
+  )
+}
+
+const GoatGrid = (props) => {
+  return(
+    <Grid
+      rows={['xxlarge']}
+      columns={['1/3', '1/3', '1/3']}
+      gap="small"
+      areas={[
+        { name: props.name1, start: [0, 0], end: [1, 0] },
+        { name: props.name2, start: [1, 0], end: [2, 0] },
+        { name: props.name3, start: [2, 0], end: [3, 0] },
+      ]}
+    >
+      <GoatColumn name={props.name1} />
+      <GoatColumn name={props.name2} />
+      <Box gridArea={props.name3} background="light-2"><Paragraph>{props.name3}</Paragraph></Box>
+    </Grid>
+  )
+}
+
+const GoatColumn = (props) => {
+  const [open, setOpen] = useState(false);
+
+  return(
+    <Box gridArea={props.name} background="brand" onClick={() => alert("Submenu item 1 selected")} onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+      <Collapsible open={open} {...props}>
+        <Paragraph>test</Paragraph>
+        <Box
+          background="light-2"
+          round="medium"
+          pad="medium"
+          align="center"
+          justify="center"
+        >
+          <Text>This is a box inside a Collapsible component</Text>
+        </Box>
+      </Collapsible>
+      <Paragraph>{props.name}</Paragraph>
     </Box>
   )
 }
